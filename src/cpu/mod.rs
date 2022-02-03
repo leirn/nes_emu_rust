a5455b76da4dd5b91586c283ef6e7d274b5754de
leirn <laurent@vromman.org>
2022-02-03 22:07:03 +0100

    CPU memory helpers + ADC

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -7,7 +7,7 @@ pub struct Cpu {
     x_register: u8,
     y_register: u8,
     program_counter: u16,
-    stack_pointer: u16,
+    stack_pointer: u8,
 
     // Flags
     negative: bool,
@@ -19,7 +19,7 @@ pub struct Cpu {
     carry: bool,
 
     // Instructions calls
-    instructions: HashMap<u8, fn(&Cpu) -> (u16, u32) >,
+    instructions: HashMap<u8, fn(&mut Cpu) -> (u16, u32) >,
 
     // Other states
     total_cycles: u32,
@@ -62,10 +62,18 @@ impl Cpu {
         for i in 0..=255 {
             self.instructions.insert(i, Cpu::dummy);
         }
-        self.instructions.insert(10, Cpu::dummy);
+        // ADC
+        self.instructions.insert(0x61, Cpu::fn_0x61);
+        self.instructions.insert(0x65, Cpu::fn_0x65);
+        self.instructions.insert(0x69, Cpu::fn_0x69);
+        self.instructions.insert(0x6d, Cpu::fn_0x6d);
+        self.instructions.insert(0x71, Cpu::fn_0x71);
+        self.instructions.insert(0x75, Cpu::fn_0x75);
+        self.instructions.insert(0x79, Cpu::fn_0x79);
+        self.instructions.insert(0x7d, Cpu::fn_0x7d);
         
     }
-    fn dummy(&self) -> (u16, u32) {
+    fn dummy(&mut self) -> (u16, u32) {
         (0, 0)
     }
     /// CPU initialisation function
@@ -74,7 +82,7 @@ impl Cpu {
 
         self.populate_instructions_vector();
 
-        // Default is equivalent to JMP ($FFFC)
+        // Fnault is equivalent to JMP ($FFFC)
         self.program_counter = entry_point.unwrap_or(MEMORY.lock().unwrap().read_rom_16(0xfffc));
 
         //Start sequence push stack three time
@@ -161,11 +169,284 @@ impl Cpu {
         self.negative =     ((status_register >> 7) & 1) != 0;
     }
 
+    /// Push value into stack
     fn push(&mut self, value:u8) {
-
+        MEMORY.lock().unwrap().write_rom(0x0100 | (self.stack_pointer as u16), value);
+        self.stack_pointer = self.stack_pointer - 1; // Will eventually overflow on purpose
     }
 
+    /// Pop/Pull value from stack
     fn pull(&mut self) -> u8 {
-        0
+        self.stack_pointer = self.stack_pointer + 1; // Will eventually overflow on purpose
+        MEMORY.lock().unwrap().read_rom(0x0100 | (self.stack_pointer as u16))
+    }
+
+    /// Get 8 bit immediate value on PC + 1
+    fn get_immediate(&mut self) -> u8 {
+        MEMORY.lock().unwrap().read_rom(self.program_counter+1)
+    }
+
+    /// Write val into Zero Page memory. Address is given as opcode 1-byte argument
+    fn set_zero_page(&mut self, value: u8) {
+        MEMORY.lock().unwrap().write_rom(self.get_zero_page_address(), value)
+    }
+
+    /// Get ZeroPage address to be used for current opcode. Alias to get_immediate
+    fn get_zero_page_address(&mut self) -> u16 {
+         self.get_immediate() as u16
+    }
+
+    /// Get val from Zero Page MEMORY. Address is given as opcode 1-byte argument
+    fn get_zero_page_value(&mut self) -> u8 {
+        let address = self.get_immediate() as u16;
+        MEMORY.lock().unwrap().read_rom(address)
+    }
+
+    /// Write val into Zero Page MEMORY. Address is given as opcode 1-byte argument and X register
+    fn set_zero_page_x(&mut self, value: u8) {
+        MEMORY.lock().unwrap().write_rom(self.get_zero_page_x_address(), value);
+    }
+
+    /// Get ZeroPage address to be used for current opcode and X register
+    fn get_zero_page_x_address(&mut self) -> u16 {
+        ((MEMORY.lock().unwrap().read_rom(self.program_counter+1) + self.x_register) & 255) as u16
+    }
+
+    /// Get value at ZeroPage address to be used for current opcode and X register
+    fn get_zero_page_x_value(&mut self) -> u8 {
+        let address = self.get_zero_page_x_address();
+        MEMORY.lock().unwrap().read_rom(address)
+    }
+
+    /// Write val into Zero Page MEMORY. Address is given as opcode 1-byte argument and Y register
+    fn set_zero_page_y(&mut self, value: u8) {
+        MEMORY.lock().unwrap().write_rom(self.get_zero_page_y_address(), value)
+    }
+
+    /// Get ZeroPage address to be used for current opcode and Y register
+    fn get_zero_page_y_address(&mut self) -> u16 {
+          ((MEMORY.lock().unwrap().read_rom(self.program_counter+1) + self.y_register) & 255) as u16
+    }
+
+    /// Get value at ZeroPage address to be used for current opcode and Y register
+    fn get_zero_page_y_value(&mut self) -> u8 {
+        let address = self.get_zero_page_y_address();
+        MEMORY.lock().unwrap().read_rom(address)
+    }
+
+    /// Write val into MEMORY. Address is given as opcode 2-byte argument
+    fn set_absolute(&mut self, value: u8) {
+        MEMORY.lock().unwrap().write_rom(self.get_absolute_address(), value)
+    }
+
+    /// Get address given as opcode 2-byte argument
+    fn get_absolute_address(&mut self) -> u16 {
+         MEMORY.lock().unwrap().read_rom_16(self.program_counter+1)
+    }
+
+    /// Get val from MEMORY. Address is given as opcode 2-byte argument
+    fn get_absolute_value(&mut self) -> u8 {
+        let address = self.get_absolute_address();
+        MEMORY.lock().unwrap().read_rom(address)
+    }
+
+    /// Write val into MEMORY. Address is given as opcode 2-byte argument and X register
+    /// additionnal is boolean to fnine if this instruction will require extra cycles on page crossing
+    fn set_absolute_x(&mut self, value: u8, is_additionnal: Option<bool>) {
+        let additionnal = is_additionnal.unwrap_or(true);
+        MEMORY.lock().unwrap().write_rom(self.get_absolute_x_address(Some(additionnal)), value);
+    }
+
+    /// Get address given as opcode 2-byte argument and X register
+    fn get_absolute_x_address(&mut self, is_additionnal: Option<bool>) -> u16 {
+        let additionnal = is_additionnal.unwrap_or(true);
+        let address = MEMORY.lock().unwrap().read_rom_16(self.program_counter+1);
+        let target_address = address + self.x_register as u16;
+        if  additionnal && address & 0xFF00 != target_address & 0xff00 {
+            self.additionnal_cycles += 1;
+        }
+        target_address
+    }
+
+    /// Get val from MEMORY. Address is given as opcode 2-byte argument and X register
+    fn get_absolute_x_value(&mut self, is_additionnal: Option<bool>) -> u8 {
+        let additionnal = is_additionnal.unwrap_or(true);
+        let address = self.get_absolute_x_address(Some(additionnal));
+        MEMORY.lock().unwrap().read_rom(address)
+    }
+
+    /// Write val into MEMORY. Address is given as opcode 2-byte argument and Y register
+    fn set_absolute_y(&mut self, value: u8, is_additionnal: Option<bool>) {
+        let additionnal = is_additionnal.unwrap_or(true);
+        MEMORY.lock().unwrap().write_rom(self.get_absolute_y_address(Some(additionnal)), value);
+    }
+
+    /// Get address given as opcode 2-byte argument and Y register
+    fn get_absolute_y_address(&mut self, is_additionnal: Option<bool>)-> u16 {
+        let additionnal = is_additionnal.unwrap_or(true);
+        let address = MEMORY.lock().unwrap().read_rom_16(self.program_counter+1);
+        let target_address = address + self.y_register as u16;
+        if additionnal && address & 0xff00 != target_address & 0xff00 {
+            self.additionnal_cycles += 1;
+        }
+        target_address
+    }
+
+    /// Get val from MEMORY. Address is given as opcode 2-byte argument and Y register
+    fn get_absolute_y_value(&mut self, is_additionnal: Option<bool>)-> u8 {
+        let additionnal = is_additionnal.unwrap_or(true);
+        let address = self.get_absolute_y_address(is_additionnal);
+        MEMORY.lock().unwrap().read_rom(address)
+    }
+
+    /// Get indirect address given as opcode 2-byte argument and X register
+    fn get_indirect_x_address(&mut self) -> u16 {
+        let address = self.get_zero_page_x_address();
+        MEMORY.lock().unwrap().read_rom_16_no_crossing_page(address)
+    }
+
+    /// Get val from MEMORY. Indirect address is given as opcode 2-byte argument and X register
+    fn get_indirect_x_value(&mut self) -> u8 {
+        let address = self.get_indirect_x_address();
+        MEMORY.lock().unwrap().read_rom(address)
+    }
+
+    /// Write val into MEMORY. Indirect address is given as opcode 2-byte argument and X register/// 
+    fn set_indirect_x(&mut self, value: u8) {
+        MEMORY.lock().unwrap().write_rom(self.get_indirect_x_address(), value);
+    }
+
+    /// Get indirect address given as opcode 2-byte argument and Y register
+    fn get_indirect_y_address(&mut self, is_additionnal: Option<bool>) -> u16 {
+        let additionnal = is_additionnal.unwrap_or(true);
+        let address = self.get_zero_page_address();
+        let address = MEMORY.lock().unwrap().read_rom_16_no_crossing_page(address);
+        let target_address = address + self.y_register as u16;
+        if additionnal && address & 0xff00 != target_address & 0xff00 {
+            self.additionnal_cycles += 1;
+        }
+        target_address
+    }
+
+    /// Get val from MEMORY. Indirect address is given as opcode 2-byte argument and Y register
+    fn get_indirect_y_value(&mut self, is_additionnal: Option<bool>) -> u8 {
+        let additionnal = is_additionnal.unwrap_or(true);
+        let address = self.get_indirect_y_address(Some(additionnal));
+        MEMORY.lock().unwrap().read_rom(address)
+    }
+
+    /// Write val into MEMORY. Indirect address is given as opcode 2-byte argument and Y register
+    fn set_indirect_y(&mut self, value: u8, is_additionnal: Option<bool>) {
+        let additionnal = is_additionnal.unwrap_or(true);
+        MEMORY.lock().unwrap().write_rom(self.get_indirect_y_address(Some(additionnal)), value);
+    }
+
+    /// Sets flags N and Z according to value
+    fn set_flags_nz(&mut self, value: u8) {
+        self.set_negative(value);
+        self.set_zero(value);
+    }
+
+    ///  Set Negative Flag according to value
+    fn set_negative(&mut self, value: u8) {
+        if value < 0 {
+            self.negative = false;
+        }
+        else {
+            self.negative = (value >> 7) != 0;
+        }
+    }
+
+    ///  Set Zero Flag according to value
+    fn set_zero(&mut self, value: u8) {
+        self.zero = value == 0;
+    }
+
+    /// Perform ADC operation for val
+    fn adc(&mut self, value: u8) {
+        let adc:u16 = (value as u16) + (self.accumulator as u16) + (self.carry as u16);
+        self.carry = ((adc >> 8) & 1) != 0;
+        let result:u8 = (0xff & adc) as u8;
+
+        self.overflow = (!! ((self.accumulator ^ result) & (value ^ result) & 0x80)) != 0;
+        self.accumulator = result;
+        self.set_flags_nz(self.accumulator);
+    }
+
+    /// Function call for ADC #$xx. Immediate
+    fn fn_0x69(&mut self) -> (u16, u32) {
+        let immediate = self.get_immediate();
+        self.adc(immediate);
+        (2, 2)
+    }
+    
+    /// Function call for ADC $xx. Zero Page
+    fn fn_0x65(&mut self) -> (u16, u32) {
+        let zeropage = self.get_zero_page_value();
+        self.adc(zeropage);
+        (2, 3)
+    }
+
+    /// Function call for ADC $xx, X. Zero Page, X
+    fn fn_0x75(&mut self) -> (u16, u32) {
+        let zeropage = self.get_zero_page_x_value();
+        self.adc(zeropage);
+        (2, 4)
+    }
+
+    /// Function call for ADC $xxxx. Absolute
+    fn fn_0x6d(&mut self) -> (u16, u32) {
+        let absolute = self.get_absolute_value();
+        self.adc(absolute);
+        (3, 4)
+    }
+
+    /// Function call for ADC $xxxx, X. Absolute, X
+    fn fn_0x7d(&mut self) -> (u16, u32) {
+        let absolute = self.get_absolute_x_value(Some(true));
+        self.adc(absolute);
+        (3, 4)
+    }
+
+    /// Function call for ADC $xxxx, X. Absolute, X
+    fn fn_0x7d_with_no_additionnal_cycles(&mut self) -> (u16, u32) {
+        let absolute = self.get_absolute_x_value(Some(false));
+        self.adc(absolute);
+        (3, 4)
+    }
+
+    /// Function call for ADC $xxxx, Y. Absolute, Y
+    fn fn_0x79(&mut self) -> (u16, u32) {
+        let absolute = self.get_absolute_y_value(Some(true));
+        self.adc(absolute);
+        (3, 4)
+    }
+
+    /// Function call for ADC $xxxx, Y. Absolute, Y
+    fn fn_0x79_with_no_additionnal_cycles(&mut self) -> (u16, u32) {
+        let absolute = self.get_absolute_y_value(Some(false));
+        self.adc(absolute);
+        (3, 4)
+    }
+
+    /// Function call for ADC ($xx, X). Indirect, X
+    fn fn_0x61(&mut self) -> (u16, u32) {
+        let indirect = self.get_indirect_x_value();
+        self.adc(indirect);
+        (2, 6)
+    }
+
+    /// Function call for ADC ($xx), Y. Indirect, Y
+    fn fn_0x71(&mut self) -> (u16, u32) {
+        let indirect = self.get_indirect_y_value(Some(true));
+        self.adc(indirect);
+        (2, 5)
+    }
+
+    /// Function call for ADC ($xx), Y. Indirect, Y
+    fn fn_0x71_with_no_additionnal_cycles(&mut self) -> (u16, u32) {
+        let indirect = self.get_indirect_y_value(Some(false));
+        self.adc(indirect);
+        (2, 5)
     }
 }
