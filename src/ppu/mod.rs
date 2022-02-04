fb8b3259047eb9d648e0c8444548288f684e0026
leirn <laurent@vromman.org>
2022-02-04 13:42:06 +0100

    Create main PPU attributes

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -1,19 +1,79 @@
+//! PPU Component of the NES
+//! NSTC implementation
 mod screen;
 
 pub struct Ppu {
     screen: screen::Screen,
+    
+    // internal registers
+    register_v: u16,    // Current VRAM address, 15 bits
+    register_t: u16,    // Temporary VRAM address, 15 bits. Can be thought of as address of top left onscreen tile
+    register_x: u8,     // Fine X Scroll, 3 bits
+    register_w: bool,   // First or second write toggle, 1 bit
+    
+    // Sprite registersself.primary_oam = bytearray(b'\0' * 0x100)
+    secondary_oam: [u8; 0x40],
+    sprite_count: u8,
+    sprite_fetcher_count: u8,
+    secondary_oam_pointer: u8,
+
+    // Cycle management
+    col: u16,
+    line: u16,
+    is_odd_frame: bool,
+
+    // general registers
+    ppuctrl: u8,
+    ppumask: u8,
+    ppustatus: u8,
+    oamaddr: u8,
+    ppuscroll: u16,
+    ppuaddr: u8,
+    ppudata: u8,
+    vram: [u8; 0x2000],
+    palette_vram: [u8; 0x20],
 }
 
 unsafe impl Sync for Ppu {}
 unsafe impl Send for Ppu {}
 
 impl Ppu {
+    /// Instantiate the PPU
     pub fn new() -> Ppu {
         Ppu {
-            screen : screen::Screen::new()
+            screen : screen::Screen::new(),
+    
+            // internal registers
+            register_v: 0,      // Current VRAM address, 15 bits
+            register_t: 0,      // Temporary VRAM address, 15 bits. Can be thought of as address of top left onscreen tile
+            register_x: 0,      // Fine X Scroll, 3 bits
+            register_w: false,  // First or second write toggle, 1 bit
+
+            // Sprite registersself.primary_oam = bytearray(b'\0' * 0x100)
+            secondary_oam: [0; 0x40],
+            sprite_count: 0,
+            sprite_fetcher_count: 0,
+            secondary_oam_pointer: 0,
+
+            // Cycle management
+            col: 0,
+            line: 0,
+            is_odd_frame: true,
+
+            // general registers
+            ppuctrl: 0,
+            ppumask: 0,
+            ppustatus: 0b10100000,
+            oamaddr: 0,
+            ppuscroll: 0,
+            ppuaddr: 0,
+            ppudata: 0,
+            vram: [0; 0x2000],
+            palette_vram: [0; 0x20],
         }
     }
 
+    /// Start the PPU at NES start up
     pub fn start(&mut self) {
         self.screen.start();
 
@@ -25,6 +85,7 @@ impl Ppu {
         self.screen.present();
     }
 
+    /// Execute next instruction
     pub fn next(&self) {
         
     }
