68003a1a6bfd65d0e3df9439d6bad9553775000f
leirn <laurent@vromman.org>
2022-02-04 13:54:30 +0100

    Initial documentation

--- src/apu/mod.rs
+++ src/apu/mod.rs
@@ -1,20 +1,22 @@
+//! APU Component
 pub struct Apu {
 }
 
-
 unsafe impl Sync for Apu {}
 
-
 impl Apu {
+    /// Instantiate APU component
     pub fn new() -> Apu {
         Apu {
         }
     }
 
+    /// Read APU registers
     pub fn read_registers(&mut self, address: u16) -> u8 {
         0
     }
 
+    /// Write APU registers
     pub fn write_registers(&mut self, address: u16, value: u8) {
         
     }
