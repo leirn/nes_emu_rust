0d25df2ad3ae355145ad5a500559164b85aaf960
leirn <laurent@vromman.org>
2022-02-04 14:32:58 +0100

    Prepare for frame update

--- src/nes_emulator/mod.rs
+++ src/nes_emulator/mod.rs
@@ -2,9 +2,10 @@
 use crate::components::{CPU, PPU};
 
 pub struct NesEmulator {
-    is_nmi:bool,
-    is_irq:bool,
-    pause:bool,
+    is_nmi: bool,
+    is_irq: bool,
+    pause: bool,
+    is_frame_updated: bool;
     pub sdl_context: sdl2::Sdl, 
 }
 
@@ -18,6 +19,7 @@ impl NesEmulator {
             is_nmi: false,
             is_irq: false,
             pause: false,
+            is_frame_updated: false,
             sdl_context: _sdl_context,
         }
     }
@@ -43,11 +45,15 @@ impl NesEmulator {
                     CPU.lock().unwrap().irq();
                 }
 
-
                 CPU.lock().unwrap().next();
                 PPU.lock().unwrap().next();
                 PPU.lock().unwrap().next();
                 PPU.lock().unwrap().next();
+                
+                if self.is_frame_updated {
+                    self.clock.tick(60);
+                    self.is_frame_updated = false;
+                }
             }
 
             let mut event_pump = self.sdl_context.event_pump().unwrap();
@@ -77,5 +83,10 @@ impl NesEmulator {
     /// Raises an IRQ interrupt
     pub fn raise_irq(&mut self) {
         self.is_irq = true;
+    }
+
+    /// Set is_frame_updated to true
+    pub fn set_frame_updated(&mut self) {
+        self.is_frame_updated = true;
     }
 }
