e61ca0ae7dc10898f249a4fbebec6cbf99e5f3e6
Laurent VROMMAN <laurent@vromman.org>
2022-02-11 19:04:29 +0100

    Ajout de la gestion des arguments + fichier de configuration pour l'exécution des tests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-#[macro_use]
+extern crate argparse;
+use argparse::{ArgumentParser, StoreTrue, Store};
+
+use config::Config;
 mod cartridge;
 mod apu;
 mod cpu;
@@ -7,7 +10,37 @@ mod memory;
 mod nes_emulator;
 
 fn main() {
-    let rom_file:String = std::env::args().nth(1).expect("No file given");
+    let mut verbose = false;
+    let mut rom_file = "";
+    let mut test_name: Option<String> = None;
+    { // this block limits scope of borrows by ap.refer() method
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Yet another NES Emulator in Rust");
+        ap.refer(&mut verbose)
+            .add_option(&["-v", "--verbose"], StoreTrue,
+            "Be verbose");
+        ap.refer(&mut test_name)
+            .add_option(&["-t", "--test"], Store,
+            "Launch a test instead of classic ROM. Tests are defined in config.yaml");
+        ap.refer($mut rom_file)
+            .add_argument("rom_file", Store,
+            "File path to ROM File");
+        ap.parse_args_or_exit();
+    }
+
+    let settings = Config::builder()
+        // Add in `config.yaml`
+        .add_source(config::File::with_name("config.yaml"))
+        .build()
+        .unwrap();
+
+    // Print out our settings (as a HashMap) for testing purpose
+    println!(
+        "{:?}",
+        settings
+            .try_deserialize::<HashMap<String, String>>()
+            .unwrap()
+    );
 
     let mut emulator = nes_emulator::NesEmulator::new(rom_file);
     emulator.start();
