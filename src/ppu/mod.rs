0ecb276ebd50e7c30db8bc0971e3042b0ab4565a
leirn <laurent@vromman.org>
2022-02-13 21:40:14 +0100

    Add PPU read and write memory

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -12,6 +12,7 @@ pub struct Status{
 
 pub struct Ppu {
     screen: screen::Screen,
+    cartridge: Rc<RefCell<Cartridge>>,
 
     // internal registers
     register_v: u16,    // Current VRAM address, 15 bits
@@ -20,8 +21,8 @@ pub struct Ppu {
     register_w: bool,   // First or second write toggle, 1 bit
 
     // Sprite registers
-    primary_oam: [u8; 0x100],
-    secondary_oam: [u8; 0x40],
+    primary_oam: Vec<u8>,
+    secondary_oam: Vec<u8>,
     sprite_count: u8,
     sprite_fetcher_count: u8,
     secondary_oam_pointer: u8,
@@ -39,8 +40,8 @@ pub struct Ppu {
     ppuscroll: u16,
     ppuaddr: u8,
     ppudata: u8,
-    vram: [u8; 0x2000],
-    palette_vram: [u8; 0x20],
+    vram: Vec<u8>,
+    palette_vram: Vec<u8>,
 }
 
 impl Ppu {
@@ -48,6 +49,7 @@ impl Ppu {
     pub fn new(cartridge: Rc<RefCell<Cartridge>>, sdl_context: Rc<RefCell<sdl2::Sdl>>) -> Ppu {
         Ppu {
             screen : screen::Screen::new(sdl_context),
+            cartridge: cartridge,
 
             // internal registers
             register_v: 0,      // Current VRAM address, 15 bits
@@ -56,8 +58,8 @@ impl Ppu {
             register_w: false,  // First or second write toggle, 1 bit
 
             // Sprite registers
-            primary_oam: [0; 0x100],
-            secondary_oam: [0; 0x40],
+            primary_oam: Vec::with_capacity(0x100),
+            secondary_oam: Vec::with_capacity(0x40),
             sprite_count: 0,
             sprite_fetcher_count: 0,
             secondary_oam_pointer: 0,
@@ -75,8 +77,8 @@ impl Ppu {
             ppuscroll: 0,
             ppuaddr: 0,
             ppudata: 0,
-            vram: [0; 0x2000],
-            palette_vram: [0; 0x20],
+            vram: Vec::with_capacity(0x2000),
+            palette_vram: Vec::with_capacity(0x20),
         }
     }
 
@@ -96,6 +98,50 @@ impl Ppu {
     /// Execute next instruction
     pub fn next(&self) {
 
+    }
+
+    /// lecture des addresses PPU Memory map
+    ///
+    /// 0x0000 to 0x2000 - 1 : Pattern table
+    /// 0x2000 to 0x3000 - 1 : Nametable
+    /// 0x3000 to 0x3eff :  Nametable Mirror
+    /// 0x3f00 to 0x3f20 - 1 : Palette ram index
+    /// 0x3f20 to 0x3fff = Palette ram mirror
+    fn read_ppu_memory(&mut self, address: u16) -> u8 {
+        match address {
+            0..=0x1fff => self.cartridge.borrow_mut().read_chr_rom(address),
+            0x2000..=0x2fff => self.vram[(address - 0x2000) as usize],
+            0x3000..=0x3eff => self.vram[(address - 0x3000) as usize],
+            0x3f00..=0x3fff => {let mut palette_address = 0;
+                if address % 4 != 0 {
+                    palette_address = address % 0x20;
+                }
+                self.palette_vram[palette_address as usize]
+            },
+            _ => panic!("Out of PPU memory range, address : {:04x}", address),
+        }
+    }
+
+    /// ecriture des addresses PPU Memory map
+    ///
+    /// 0x0000 to 0x2000 - 1 : Pattern table
+    /// 0x2000 to 0x3000 - 1 : Nametable
+    /// 0x3000 to 0x3eff :  Nametable Mirror
+    /// 0x3f00 to 0x3f20 - 1 : Palette ram index
+    /// 0x3f20 to 0x3fff = Palette ram mirror
+    fn write_ppu_memory(&mut self, address: u16, value: u8) {
+        match address {
+            0..=0x1fff => self.cartridge.borrow_mut().write_chr_rom(address, value),
+            0x2000..=0x2fff => self.vram[(address - 0x2000) as usize] = value,
+            0x3000..=0x3eff => self.vram[(address - 0x3000) as usize] = value,
+            0x3f00..=0x3fff => {let mut palette_address = 0;
+                if address % 4 != 0 {
+                    palette_address = address % 0x20;
+                }
+                self.palette_vram[palette_address as usize] = value
+            },
+            _ => panic!("Out of PPU memory range, address : {:04x}", address),
+        }
     }
 
     pub fn read_0x2002(&self) -> u8 {
