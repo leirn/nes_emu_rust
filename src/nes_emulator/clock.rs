e5b812c8b1b31a41461bbedf533ea490a3b0d28b
Laurent VROMMAN <laurent@vromman.org>
2022-02-15 17:12:31 +0100

    Clock typos

--- src/nes_emulator/clock.rs
+++ src/nes_emulator/clock.rs
@@ -6,7 +6,7 @@ use std::time::{SystemTime, UNIX_EPOCH};
 use std::time::Duration;
 
 pub struct Clock {
-    frame_history:VecDeque<u64>,
+    frame_history:VecDeque<u128>,
     start: std::time::SystemTime,
 }
 
@@ -14,7 +14,7 @@ impl Clock {
     /// Instantiate new clock
     pub fn new() -> Clock{
         Clock{
-            frame_history: Vec::with_capacity(10),
+            frame_history: VecDeque::new(),
             start: SystemTime::now(),
         }
     }
@@ -22,7 +22,7 @@ impl Clock {
     /// Tick at each frame and wait to reach the target frame rate
     pub fn tick(&mut self, target_frame_rate: u32) {
         let since_the_epoch = self.start.duration_since(UNIX_EPOCH).unwrap();
-        self.frame_history.push_back(since_the_epoch.from_nanos());
+        self.frame_history.push_back(since_the_epoch.as_nanos());
         if self.frame_history.len() > 10 {
             self.frame_history.pop_front();
         }
@@ -30,6 +30,6 @@ impl Clock {
 
     /// Get current fps
     pub fn get_fps(&self) -> f64 {
-        (self.frame_history.back() - self.frame_history.front()) / 10f64
+        1000000000f64 * 10f64 / (self.frame_history.back() - self.frame_history.front())
     }
 }
