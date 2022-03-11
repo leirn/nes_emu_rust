7ad7c5df39bf453737d51ff62deb1a74eb88ca02
leirn <laurent@vromman.org>
2022-03-11 17:00:26 +0100

    Add types for rust FMT

--- tests/clock.rs
+++ tests/clock.rs
@@ -4,12 +4,12 @@ mod tests {
     fn clock_tick() {
         //! This test checks if a tick duration last for the right duration with a 5% tolerance
         let mut clock  = crate::nes_emulator::clock::Clock::new(60);
-        const tolerance_margin = 0.05f64;
-        const frame_duration_nanos = 1_000_000_000f64 / 60f64;
-        let tolerance = std::time::Duration::from_nanos(frame_duration_nanos * tolerance_margin); // 5% tolerance compare to 1/60th seconds
-        let expected_duration = std::time::Duration::from_nanos(frame_duration_nanos);
-        let upper = expected_duration + tolerance;
-        let lower = expected_duration - tolerance;
+        const TOLERANCE_MARGIN: f64 = 0.05f64;
+        const FRAME_DURATION_NANOS: f64 = 1_000_000_000f64 / 60f64;
+        let tolerance: u64 = std::time::Duration::from_nanos((FRAME_DURATION_NANOS * TOLERANCE_MARGIN) as u64); // 5% tolerance compare to 1/60th seconds
+        let expected_duration: u64 = std::time::Duration::from_nanos(FRAME_DURATION_NANOS as u64);
+        let upper: u64 = expected_duration + tolerance;
+        let lower: u64 = expected_duration - tolerance;
         let now = std::time::SystemTime::now();
         self.tick();
         let elapsed = now.elapsed();
@@ -20,16 +20,16 @@ mod tests {
     #[test]
     fn clock_fps() {
         //! This test checks if the clock fps measurement give the right duration with 2 fps margin
-        const target_framerate = 60u32;
-        const target_framerate_margin = 60u32;
-        const upper = (target_framerate + target_framerate_margin) as f64;
-        const lower = (target_framerate - target_framerate_margin) as f64;
+        const TARGET_FRAMERATE: u32 = 60u32;
+        const TARGET_FRAMERATE_MARGIN: u32 = 60u32;
+        const UPPER: f64 = (TARGET_FRAMERATE + TARGET_FRAMERATE_MARGIN) as f64;
+        const LOWER: f64 = (TARGET_FRAMERATE - TARGET_FRAMERATE_MARGIN) as f64;
         let mut clock  = crate::nes_emulator::clock::Clock::new(target_framerate);
         for _i in 1..15 {
             clock.tick();
         }
         let fps = clock.get_fps();
-        assert!(fps < upper);
-        assert!(fps > lower);
+        assert!(fps < UPPER);
+        assert!(fps > LOWER);
     }
 }
